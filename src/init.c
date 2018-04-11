f55637a8e00db99b1aa554e5f7c4a60fd7c9af93
lchagnoleau <loic.chagnoleau@gmail.com>
2018-04-11 10:26:22 +0200

    save

--- src/init.c
+++ src/init.c
@@ -96,39 +96,42 @@ static void SystemClock_Config(void)
 
     /**Configure the main internal regulator output voltage
     */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
     /**Initializes the CPU, AHB and APB busses clocks
     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = 16;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLLMUL_4;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
     /**Initializes the CPU, AHB and APB busses clocks
     */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
 
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_LPUART1;
-    PeriphClkInit.Lpuart1ClockSelection = RCC_LPUART1CLKSOURCE_PCLK1;
-    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_LPUART1;
+  PeriphClkInit.Lpuart1ClockSelection = RCC_LPUART1CLKSOURCE_PCLK1;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 
     /**Configure the Systick interrupt time
     */
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
     /**Configure the Systick
     */
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-    /* SysTick_IRQn interrupt configuration */
-    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
--- src/main.c
+++ src/main.c
@@ -8,29 +8,38 @@ int main(void)
 
   uint8_t GPS_buffer[100] = {0};
   uint8_t GPS_cmp = 0;
-  uint8_t c = 0;
+  uint8_t c = 0, d = 0;
   HAL_StatusTypeDef rx_status;
 
-  while (1)
+  while(1)
   {
-      while(c != '$')
-        {
-          rx_status = HAL_UART_Receive(&hlpuart1, &c, 1, 10);
-          if(rx_status != HAL_OK)
-            trace_printf("\nrx_status = 02X", rx_status);
-        }
-      GPS_buffer[GPS_cmp++] = c;
-      while(c != '\n')
-        {
-          rx_status = HAL_UART_Receive(&hlpuart1, &c, 1, 10);
-          if(rx_status != HAL_OK)
-            trace_printf("\nrx_status = 02X", rx_status);
-          GPS_buffer[GPS_cmp++] = c;
-        }
-      GPS_cmp = 0;
-      c = 0;
+//      while(c != '$')
+//        {
+//          rx_status = HAL_UART_Receive(&hlpuart1, &c, 1, 10);
+//          if(rx_status != HAL_OK)
+//            trace_printf("\nrx_status = 02X", rx_status);
+//        }
+//      GPS_buffer[GPS_cmp++] = c;
+//      while(c != '\n')
+//        {
+//          rx_status = HAL_UART_Receive(&hlpuart1, &c, 1, 10);
+//          if(rx_status != HAL_OK)
+//            trace_printf("\nrx_status = 02X", rx_status);
+//          GPS_buffer[GPS_cmp++] = c;
+//        }
+//      GPS_cmp = 0;
+//      c = 0;
+//
+//      for(uint8_t i=0; GPS_buffer[i] != '\n'; i++)
+//        trace_printf("%c", GPS_buffer[i]);
 
-      for(uint8_t i=0; GPS_buffer[i] != '\n'; i++)
-        trace_printf("%c", GPS_buffer[i]);
+      trace_printf("\ncut GPS");
+//      HAL_GPIO_WritePin(EN_INNO_GPIO_Port, EN_INNO_Pin, GPIO_PIN_RESET);
+      for(c=0; c<500000; c++)
+        {
+          d++;
+        }
+      trace_printf("\nuncut GPS");
+//      HAL_GPIO_WritePin(EN_INNO_GPIO_Port, EN_INNO_Pin, GPIO_PIN_SET);
   }
 }
